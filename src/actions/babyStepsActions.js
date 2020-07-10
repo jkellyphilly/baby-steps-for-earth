708d0aad0df335b39f9b22ee8400668c15763f65
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-10 15:59:37 -0400

    updating fetchGoals to have an input for querying database

--- src/actions/babyStepsActions.js
+++ src/actions/babyStepsActions.js
@@ -1,14 +1,38 @@
-export const fetchGoals = () => {
+export const fetchGoals = (input) => {
   return (dispatch) => {
     // Initially, dispatch LOADING_GOALS action
     dispatch({ type: 'LOADING_GOALS' });
 
     // Then perform the fetch
     // NOTE: I have configured my API to run on port 4000.
-    fetch("http://localhost:4000/goals")
+    let configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ goal: { input: input } })
+    }
+
+    fetch(`http://localhost:4000/goals?tags=${input}`)
     .then(resp => resp.json())
     .then(responseJSON => {
       dispatch({ type: 'ADD_GOALS', goals: responseJSON.data})
     })
   }
 }
+
+// export const fetchPlans = () => {
+//   return (dispatch) => {
+//     // Initially, dispatch LOADING_PLANS action
+//     dispatch({ type: 'LOADING_PLANS' });
+//
+//     // Then perform the fetch
+//     // NOTE: I have configured my API to run on port 4000.
+//     fetch("http://localhost:4000/plans")
+//     .then(resp => resp.json())
+//     .then(responseJSON => {
+//       dispatch({ type: 'ADD_PLANS', plans: responseJSON.data})
+//     })
+//   }
+// }
